8293d0b5f6b82047abdecf81b0036ec7e6ca5731
小羊 <tlylz99@live.cn>
2021-03-27 22:35:39 +0800

    Add bootstrap-toggle@2.2.2

--- test/Website/PrimaryTest.cs
+++ test/Website/PrimaryTest.cs
@@ -17,6 +17,7 @@ namespace Website
         [InlineData("/lib/jquery/jquery.min.js", "jQuery v3.4.1")]
         [InlineData("/lib/ace/ace.min.js", "\"ace/lib/dom\"")]
         [InlineData("/lib/bootstrap/css/bootstrap.min.css", "Bootstrap v4.4.0")]
+        [InlineData("/lib/bootstrap-toggle/css/bootstrap2-toggle.min.css", "bootstrap2-toggle.css v2.2.0")]
         public async Task FileExists(string requestPath, string flagSubstring)
         {
             var response = await Client.GetAsync(requestPath);
