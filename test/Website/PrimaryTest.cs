ec6158ed8fefb14c74bbc59eb887100a2183e1dc
小羊 <tlylz99@live.cn>
2021-03-27 22:27:12 +0800

    Add twitter-bootstrap@4.4.0

--- test/Website/PrimaryTest.cs
+++ test/Website/PrimaryTest.cs
@@ -16,6 +16,7 @@ namespace Website
         [Theory]
         [InlineData("/lib/jquery/jquery.min.js", "jQuery v3.4.1")]
         [InlineData("/lib/ace/ace.min.js", "\"ace/lib/dom\"")]
+        [InlineData("/lib/bootstrap/css/bootstrap.min.css", "Bootstrap v4.4.0")]
         public async Task FileExists(string requestPath, string flagSubstring)
         {
             var response = await Client.GetAsync(requestPath);
