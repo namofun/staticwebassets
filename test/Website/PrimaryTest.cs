83d095848c25f2e87ed28f6aacea45a76084a3af
小羊 <tlylz99@live.cn>
2021-03-27 22:16:58 +0800

    Add ace@1.4.9

--- test/Website/PrimaryTest.cs
+++ test/Website/PrimaryTest.cs
@@ -15,6 +15,7 @@ namespace Website
 
         [Theory]
         [InlineData("/lib/jquery/jquery.min.js", "jQuery v3.4.1")]
+        [InlineData("/lib/ace/ace.min.js", "\"ace/lib/dom\"")]
         public async Task FileExists(string requestPath, string flagSubstring)
         {
             var response = await Client.GetAsync(requestPath);
